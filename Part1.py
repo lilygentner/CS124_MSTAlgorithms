74995a87e29cc24bef83d3d0f462faa98641bbbd
lgentner <lily@knozen.com>
2016-02-15 12:32:57 -0500

    generate both types of graphs

--- Part1.py
+++ Part1.py
@@ -1,12 +1,14 @@
 import math
 import random
 
-#find euclidian distance between two points
-def eDistance(a, b):
-    dimension = range(len(a))
-    return math.sqrt(sum([(a[i] - b[i])**2 for i in dimension]))
 
-def generateGraphMatrix(n):
+#normal graph, part1
+class Graph: 
+    def __init__(self, n):
+        self.vectors = [range(n)]
+        self.matrix = generateGraphMatrix(n)
+
+    def generateGraphMatrix(n):
     matrix = [[0 for _ in range(n)] for _ in range(n)]
     for i in range(n):
         for j in range(n):
@@ -15,12 +17,25 @@ def generateGraphMatrix(n):
             else:
                 matrix[i][j] = random.random()
                 matrix[j][i] = matrix[i][j]
-    return matrix
+    return matrix 
 
-class Graph: 
-    def __init__(self, n):
-        self.vectors = [range(n)]
-        self.matrix = generateGraphMatrix(n)
-        
     def length(self, a, b):
         return self.matrix[a][b]
+
+
+#unit distance graph
+class unitDistanceGraph:
+	def __init__(self, n, dimension):
+		self.vectors = [range(n)]
+		self.vectorLocationDict = generateUnitDistances(n, dimension)
+
+	def generateUnitDistances(n, dimension): 
+		vectorDict = {i: tuple(random.random() for _ in range(dimension)) for i in range(n)}
+		return vectorDict
+
+	def length(self, a, b):
+		return eDistance(self.vectors[a], self.vectors[b])
+
+	def eDistance(a, b):
+    dimension = range(len(a))
+    return math.sqrt(sum([(a[i] - b[i])**2 for i in dimension]))
